74e8222717845d3c3e1b438b117f99b7324e1085
栖白 <yit38331@gmail.com>
2026-08-27 22:47:17 +0800

    fix: render original liquid glass button demo

--- src/demos/liquid-glass-button-demo.tsx
+++ src/demos/liquid-glass-button-demo.tsx
@@ -1,9 +1,13 @@
-import { MetalButton } from "../../components/buttons";
+import { LiquidButton } from "../../components/buttons";
 
 export function LiquidGlassButtonDemo() {
   return (
     <div className="flex h-[384px] w-full items-center justify-center bg-white p-6">
-      <MetalButton />
+      <div className="relative h-[200px] w-full max-w-[800px]">
+        <LiquidButton className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
+          Liquid Glass
+        </LiquidButton>
+      </div>
     </div>
   );
 }
