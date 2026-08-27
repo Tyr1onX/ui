90d2ed4d5dc045d8ee5ccc53c3981bae1d584e90
栖白 <yit38331@gmail.com>
2026-08-27 17:29:37 +0800

    feat: export oceanic currents effect

--- components/effects/index.ts
+++ components/effects/index.ts
@@ -2,3 +2,5 @@ export { AuroraBackground } from "./aurora-background";
 export type { AuroraBackgroundProps } from "./aurora-background";
 export { LiquidGlass } from "./liquid-glass";
 export type { LiquidGlassProps } from "./liquid-glass";
+export { OceanicCurrents, ShaderBackground } from "./oceanic-currents";
+export type { OceanicCurrentsProps } from "./oceanic-currents";
