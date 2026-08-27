ca27ff97d02faf3a63e4cc86284d29196e38f892
栖白 <yit38331@gmail.com>
2026-08-27 22:24:52 +0800

    feat: export gooey dock

--- components/docks/index.ts
+++ components/docks/index.ts
@@ -1,2 +1,4 @@
 export { MacOSDock } from "./mac-os-dock";
 export type { MacOSDockApp, MacOSDockProps } from "./mac-os-dock";
+export { GooeyDock } from "./gooey-dock";
+export type { DockItem, GooeyDockProps } from "./gooey-dock";
