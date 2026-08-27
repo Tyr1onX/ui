4560f4485db6dc59e88fa879fe6b2d5dae1f52b8
栖白 <yit38331@gmail.com>
2026-08-27 17:26:43 +0800

    feat: export aurora background effect

--- components/effects/index.ts
+++ components/effects/index.ts
@@ -1,2 +1,4 @@
+export { AuroraBackground } from "./aurora-background";
+export type { AuroraBackgroundProps } from "./aurora-background";
 export { LiquidGlass } from "./liquid-glass";
 export type { LiquidGlassProps } from "./liquid-glass";
